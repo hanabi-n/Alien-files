0b18c7c0d82226ec4f5affb2a76ae2ae36330835
hanabi-n <53466808+hanabi-n@users.noreply.github.com>
2020-05-22 21:04:49 +0600

    Update FXMLMainMenuController.java

--- src/mainpackage/MainMenu/FXMLMainMenuController.java
+++ src/mainpackage/MainMenu/FXMLMainMenuController.java
@@ -7,12 +7,9 @@
 package mainpackage.MainMenu;
 
 import com.jfoenix.controls.JFXButton;
-import com.jfoenix.controls.JFXHamburger;
 import com.jfoenix.controls.JFXTabPane;
 import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
-import java.awt.event.ActionEvent;
-import java.awt.event.MouseEvent;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -20,32 +17,20 @@ import javafx.application.Application;
 import static javafx.application.Application.launch;
 import static javafx.application.Application.launch;
 import static javafx.application.Application.launch;
-import static javafx.application.Application.launch;
-import static javafx.application.Application.launch;
-import static javafx.application.Application.launch;
-import static javafx.application.Application.launch;
-import static javafx.application.Application.launch;
-import static javafx.application.Application.launch;
 import javafx.application.Platform;
-import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import static mainpackage.AlienMain.setIcon;
 import mainpackage.server.mainserver.ServerFX;
 import mainpackage.server.network.AlienClient;
 import mainpackage.server.network.AlienConnectionFX;
@@ -56,11 +41,38 @@ public class FXMLMainMenuController extends Application{
     @FXML private JFXTextArea messages;
     @FXML private JFXTextField input;
     @FXML private WebView wiki;
+    @FXML private MenuItem LogOut;
+    @FXML private JFXButton wikipedia;
+    @FXML private JFXButton reddit;
+    @FXML private JFXButton lenin;
     
+///////////////////////////////////////////
+    @FXML
+    void handleLenin(ActionEvent event) {
+         WebEngine webEngine = wiki.getEngine();
+         String url = "https://cyberleninka.ru";
+         webEngine.load(url);
+    }
+    
+    @FXML
+    void handleReddit(ActionEvent event) {
+         WebEngine webEngine = wiki.getEngine();
+         String url = "https://www.reddit.com";
+         webEngine.load(url);
+    }
+
+    @FXML
+    void handleWiki(ActionEvent event) {
+         WebEngine webEngine = wiki.getEngine();
+         String url = "https://ru.wikipedia.org";
+         webEngine.load(url);
+    }
+
+/////////////////////////////////////////////
     @FXML
     void inputMessages() {
         input.setOnAction(event -> {
-            String message = isServer ? "Server: " : "Client: ";
+            String message = isServer ? "Server: " : "User: ";
             message += input.getText();
             input.clear();
 
@@ -75,12 +87,6 @@ public class FXMLMainMenuController extends Application{
         });
     }
     
-    public void initialize(URL url, ResourceBundle rb){
-        final WebEngine web = wiki.getEngine();
-        String urlwiki = "https://google.com";
-        web.load(urlwiki);
-    }
-
     private final boolean isServer = false;
 
     private final AlienConnectionFX connection = isServer ? createServer() : createClient();
@@ -108,7 +114,7 @@ public class FXMLMainMenuController extends Application{
     public void stop() throws Exception {
         connection.closeConnection();
     }
-
+/////////////////////////////////////////////////////
     @Override
     public void start(Stage primaryStage) throws Exception {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
@@ -118,4 +124,69 @@ public class FXMLMainMenuController extends Application{
         launch(args);
     }
 
+/////////////////////////////////////////////////////////////
+   private Stage getStage() {
+            return (Stage) MainPane1.getScene().getWindow();
+   }    
+
+    @FXML
+    private void handleMenuExit(ActionEvent event) {
+        getStage().close();
+    }
+      
+   @FXML
+    private void handleMenuFullScreen(ActionEvent event) {
+        Stage stage = getStage();
+        stage.setFullScreen(!stage.isFullScreen());
+    }
+    
+    @FXML
+    void handleMenuAbouUs(ActionEvent event) {
+         
+        try {
+            Parent parent = FXMLLoader.load(getClass().getResource("/mainpackage/MainMenu/FXMLAboutUs.fxml"));
+            Stage stage = new Stage(StageStyle.DECORATED);
+            stage.setScene(new Scene(parent));
+            stage.setTitle("About Us");
+            setIcon(stage);
+            stage.showAndWait();
+            
+
+        }
+        catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    @FXML
+    void handleLogOut(ActionEvent event) {
+        getStage().close();
+         
+        try {
+            Parent parent = FXMLLoader.load(getClass().getResource("/mainpackage/Registration/FXMLSignIn.fxml"));
+            Stage stage = new Stage(StageStyle.DECORATED);   
+            stage.setScene(new Scene(parent));
+            stage.setTitle("Alien Files 2.0");
+            
+            setIcon(stage);
+            stage.show();
+        }
+        catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    
+    public static void setIcon(Stage stage) {
+        stage.getIcons().add(new Image("/mainpackage/Wallpapers/icon.png")); 
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
